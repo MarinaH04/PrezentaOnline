07fe3f2b99ada80ffac031a2706391e81f50144d
Marina Hanzu <hanzumarina@yahoo.com>
2019-12-17 14:21:06 +0200

    services

--- src/main/java/com/proiect/persistence/controller/UserController.java
+++ src/main/java/com/proiect/persistence/controller/UserController.java
@@ -4,11 +4,15 @@ import javax.ws.rs.core.MediaType;
 
 import java.util.List;
 
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.proiect.persistence.dao.UserDAO;
@@ -31,4 +35,48 @@ public class UserController {
 	public User getUsers(@PathParam("username") String username){
 		return userDAO.getUserByUsername(username);
 	}
+	
+	@POST
+	@Path("/insert")
+	@Produces(MediaType.APPLICATION_JSON)
+	public void createUser(String str) {
+		JSONObject jsonObj;
+		try {
+			jsonObj = new JSONObject(str);
+			String username = jsonObj.getString("username");
+			String email = jsonObj.getString("email");
+			String password = jsonObj.getString("password");
+			String tip = jsonObj.getString("tip");
+
+			userDAO.insera(username, email, password, tip);
+		}
+		catch(JSONException e) {
+			e.printStackTrace();
+			}
+	}
+	@POST
+	@Path("/update")
+	@Produces(MediaType.APPLICATION_JSON)
+	public void createUserCurs(String str) {
+		JSONObject jsonObj;
+		try {
+			jsonObj = new JSONObject(str);
+			String username = jsonObj.getString("username");
+			String denumire = jsonObj.getString("denumire");
+			
+			userDAO.update(username, denumire);
+		}
+		catch(JSONException e) {
+			e.printStackTrace();
+			}
+	}
+	
+	@DELETE
+	  @Path("{id}")
+	public void deleteUser(@PathParam("id") int user_id) {
+		userDAO.deleteUser(user_id);
+	}
+	
+	
+	
 }
